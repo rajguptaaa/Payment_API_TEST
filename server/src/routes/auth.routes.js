93d8e9be155f541f3cbe5021b5ff221d08809e36
Raj Gupta <kumaaraj11@gmail.com>
2026-03-23 11:50:31 +0530

    banking system- api testing frontend

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -6,4 +6,4 @@ router.post("/register", authController.userRegisterController);
 router.post("/login", authController.userLoginController);
 router.post("/logout", authController.userLogoutController);
 
-module.exports = router;
+module.exports = router; 
